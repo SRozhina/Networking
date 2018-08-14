f7f50e50c16827a5f15d33abc209e211bedfe1ef
Sofia Rozhina <sofia.rozhina@gmail.com>
2018-08-14 12:32:53 +0300

    Implement MoyaNetworkingService

--- Networking/main.swift
+++ Networking/main.swift
@@ -1,18 +1,21 @@
 import Foundation
 
 let application = Application()
+
+//Get all users
 application.task1()
+//Get post number 5 with user and comments
 application.task2()
+//Get all posts with user and comments
 application.task3()
+//Update user
 application.task4()
+
 
 let applicationPromised = ApplicationPromised()
 applicationPromised.task1()
 applicationPromised.task2()
 applicationPromised.task3()
 applicationPromised.task4()
-
-let networkManager = NetworkManager()
-networkManager.testFunc()
 
 RunLoop.main.run()
